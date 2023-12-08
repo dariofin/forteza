a09f83edf694f6472bccf7a09deb5a0c6b73919d
Darío Finelli <61764276+dariofin@users.noreply.github.com>
2023-12-08 00:47:14 -0300

    comenzando a manipular el DOM con js

--- js/app.js
+++ js/app.js
@@ -1,2 +1,12 @@
-const fondo = document.getElementById("contenedor-principal");
+const contenedorP = document.getElementById("contenedor-principal");
+const botonFlecha = document.getElementById("flecha-abajo");
+
 // console.log(fondo.style);
+
+// contenedorP.style.backgroundPositionY = "-200vh";
+
+function botonFlechaEvent(e) {
+	contenedorP.style.backgroundPositionY = "-200vh";
+}
+
+botonFlecha.addEventListener("click", botonFlechaEvent);
