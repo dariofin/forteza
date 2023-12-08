99229c15e7b824e535d365bedf1cc22c8b3d9ca7
Darío Finelli <61764276+dariofin@users.noreply.github.com>
2023-12-08 04:30:29 -0300

    cambios de estructura buscando OnpPage progresivo

--- js/app.js
+++ js/app.js
@@ -1,12 +1,17 @@
-const contenedorP = document.getElementById("contenedor-principal");
+const fondoImg = document.getElementById("fondo-imagen");
 const botonFlecha = document.getElementById("flecha-abajo");
-
+const flechaSVG = document.getElementById("flecha-svg");
+const headerLogo = document.getElementById("logo-central-div");
 // console.log(fondo.style);
 
 // contenedorP.style.backgroundPositionY = "-200vh";
 
 function botonFlechaEvent(e) {
-	contenedorP.style.backgroundPositionY = "-200vh";
+	// contenedorP.style.backgroundPositionY = "-200vh";
+	fondoImg.style.opacity = "0";
+	flechaSVG.style.translate = "0 50px";
+	headerLogo.style.scale = "0.8";
 }
 
 botonFlecha.addEventListener("click", botonFlechaEvent);
+console.log(botonFlecha.innerHTML);
