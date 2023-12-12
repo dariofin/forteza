5fdc4cd0066155ab0aeb5a110fcbdd5eae70227f
Darío Finelli <61764276+dariofin@users.noreply.github.com>
2023-12-12 00:51:29 -0300

    Eliminado el boton arrow

--- js/app.js
+++ js/app.js
@@ -1,17 +1,17 @@
-const fondoImg = document.getElementById("fondo-imagen");
-const botonFlecha = document.getElementById("flecha-abajo");
+const fondoImg = document.getElementById("bkground-img");
+const botonFlecha = document.getElementById("arrow-down-btn");
 const flechaSVG = document.getElementById("flecha-svg");
-const headerLogo = document.getElementById("logo-central-div");
-// console.log(fondo.style);
+const headerLogo = document.getElementById("logo-header");
+console.log(botonFlecha);
 
 // contenedorP.style.backgroundPositionY = "-200vh";
 
 function botonFlechaEvent(e) {
 	// contenedorP.style.backgroundPositionY = "-200vh";
 	fondoImg.style.opacity = "0";
-	flechaSVG.style.translate = "0 50px";
-	headerLogo.style.scale = "0.8";
+	// flechaSVG.style.translate = "0 50px";
+	// headerLogo.style.scale = "0.8";
 }
 
 botonFlecha.addEventListener("click", botonFlechaEvent);
-console.log(botonFlecha.innerHTML);
+// console.log(botonFlecha.innerHTML);
